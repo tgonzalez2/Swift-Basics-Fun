d2241327e78c099c8764e75b8b1a2d3ca3e12978
Tyler <tgonzalez2@zagmail.gonzaga.edu>
2020-09-04 17:09:25 -0700

    Initial Commit

---
+++ Swift Basics Fun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics Fun
+//
+//  Created by Gonzalez, Tyler D on 9/4/20.
+//  Copyright © 2020 Gonzalez, Tyler D. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
